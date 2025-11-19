32db73cbf97904b7531b229258b2371e2680a4ff
Sebastian <sebastianwoj1010@gmail.com>
2025-11-19 17:23:41 +0100

    Commit and Push Ubungsblatt 5

--- Alles/h2/H2_main.java
+++ Alles/h2/H2_main.java
@@ -2,7 +2,7 @@ package h2;
 
 public class H2_main {
     public static void main(String[] arg) {
-        int n = 1229 ;
+        int n = 1299 ;
         int digits =  0;
         int a [] = new int [9];
         String print = "";
@@ -10,7 +10,7 @@ public class H2_main {
 
         System.out.println("The Number: " + n + " has " + digits + " digits.");
 
-        for(int i = 0 ; i <= digits; i++){
+        for(int i = 0 ; i < digits; i++){
         a[a.length - 1 - i] = n % 10;
         n = n/10;
         }
